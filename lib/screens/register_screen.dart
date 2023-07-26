ce90f8ef881b2b01d7428f10d73c52a56061f6c3
Yared Yaregal <ytyalem@yahoo.com>
2023-07-26 22:52:30 +0200

    finalizing update

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:splash/services/firebase_service.dart';
+import 'package:splash/utils/globals.dart';
 
 import 'account_screen.dart';
 
@@ -22,9 +23,18 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
   final _businessName = TextEditingController();
   final _contactNumber = TextEditingController();
   final _email = TextEditingController();
-  final _productName = TextEditingController();
+  final _fullNameController = TextEditingController();
+  final _birthDateController = TextEditingController();
+  final _cityController = TextEditingController();
+  final _streetController = TextEditingController();
+  final _houseNumberController = TextEditingController();
+  final _nearByController = TextEditingController();
+  final _areaCodeController = TextEditingController();
+  final _phone2Controller = TextEditingController();
+  final _usernameController = TextEditingController();
+
   String? _bName;
-  String? _product;
+  String? _city;
   String? _shopImageUrl;
   String? _logoImageUrl;
   XFile? _shopImage;
@@ -40,7 +50,8 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
       validator,
       Widget? prefixIcon,
       String? prefixlabel,
-      void Function(String)? changed}) {
+      void Function(String)? changed,
+      void Function()? tapped}) {
     return TextFormField(
       controller: controller,
       keyboardType: type,
@@ -48,6 +59,7 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
           labelText: label, prefixText: prefixlabel, prefixIcon: prefixIcon),
       validator: validator,
       onChanged: changed,
+      onTap: tapped,
     );
   }
 
@@ -140,21 +152,6 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
                               ),
                             ),
                           ),
-                    SizedBox(
-                      height: 80,
-                      child: AppBar(
-                        elevation: 0,
-                        backgroundColor: Colors.transparent,
-                        actions: [
-                          IconButton(
-                            onPressed: () {
-                              FirebaseAuth.instance.signOut();
-                            },
-                            icon: const Icon(Icons.exit_to_app),
-                          ),
-                        ],
-                      ),
-                    ),
                     Align(
                       alignment: Alignment.bottomCenter,
                       child: Padding(
@@ -169,22 +166,19 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
                                   });
                                 });
                               },
-                              child: Card(
-                                elevation: 4,
-                                child: _logoImage == null
-                                    ? const SizedBox(
-                                        height: 50,
-                                        width: 50,
-                                        child: Center(child: Text("+")),
-                                      )
-                                    : SizedBox(
-                                        height: 50,
-                                        width: 50,
-                                        child: Image.file(
-                                          File(_logoImage!.path),
-                                        ),
+                              child: const Card(
+                                  elevation: 4,
+                                  child: SizedBox(
+                                    height: 50,
+                                    width: 50,
+                                    child: Card(
+                                      elevation: 4,
+                                      child: Icon(
+                                        Icons.location_on_rounded,
+                                        color: Colors.blue,
                                       ),
-                              ),
+                                    ),
+                                  )),
                             ),
                             const SizedBox(
                               width: 10,
@@ -208,25 +202,21 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
                 child: Column(
                   children: [
                     _formField(
-                        controller: _businessName,
-                        label: "Business Name",
-                        type: TextInputType.text,
-                        prefixIcon: const Icon(Icons.badge_outlined),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Name is required';
-                          }
-                          return null;
-                        },
-                        changed: (value) {
-                          setState(() {
-                            _bName = value;
-                          });
-                        }),
+                      controller: _fullNameController,
+                      label: "Full Name",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.badge_outlined),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Name is required';
+                        }
+                        return null;
+                      },
+                    ),
                     _formField(
                         controller: _contactNumber,
                         label: "Phone Number",
-                        prefixlabel: '+43',
+                        prefixlabel: '09',
                         prefixIcon: const Icon(Icons.phone),
                         type: TextInputType.phone,
                         validator: (value) {
@@ -236,50 +226,106 @@ class _CustomRegisterScreenState extends State<CustomRegisterScreen> {
                           return null;
                         }),
                     _formField(
-                        controller: _email,
-                        label: "Email ",
-                        prefixIcon: const Icon(Icons.email_outlined),
+                      controller: _email,
+                      label: "Email ",
+                      prefixIcon: const Icon(Icons.email_outlined),
+                      type: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Email is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .88,
+                      child: DropdownButtonFormField(
+                        value: _city,
+                        hint: const Text("City"),
+                        icon: const Icon(Icons.location_city_outlined),
+                        items: CITIES
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _city = value;
+                          });
+                        },
+                      ),
+                    ),
+                    _formField(
+                        controller: _birthDateController,
+                        label: "Birth Date",
                         type: TextInputType.text,
+                        prefixIcon: const Icon(Icons.date_range),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
-                            return 'Email is required';
+                            return 'Birth date is required';
                           }
                           return null;
-                        }),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        const Text("Do you have product"),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Expanded(
-                          child: DropdownButtonFormField(
-                            value: _product,
-                            hint: const Text("Select one"),
-                            items: <String>['Yes', 'No']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            onChanged: (value) {
+                        },
+                        tapped: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                          ).then((selectedDate) {
+                            if (selectedDate != null) {
                               setState(() {
-                                _product = value;
+                                _birthDateController.text =
+                                    selectedDate.toIso8601String();
                               });
-                            },
-                          ),
-                        )
-                      ],
+                            }
+                          });
+                        }),
+                    _formField(
+                      controller: _streetController,
+                      label: "Address",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.location_history),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Address is required';
+                        }
+                        return null;
+                      },
                     ),
-                    if (_product == "Yes")
-                      _formField(
-                        controller: _productName,
-                        label: "Product Name ",
-                        prefixIcon: const Icon(Icons.precision_manufacturing),
-                        type: TextInputType.text,
-                      ),
+                    _formField(
+                      controller: _areaCodeController,
+                      label: "Area Code",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.pin_outlined),
+                    ),
+                    _formField(
+                      controller: _houseNumberController,
+                      label: "House #",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.house_outlined),
+                    ),
+                    _formField(
+                      controller: _nearByController,
+                      label: "Near by",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.travel_explore_outlined),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Near by is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    _formField(
+                      controller: _phone2Controller,
+                      label: "Phone 2",
+                      type: TextInputType.text,
+                      prefixIcon: const Icon(Icons.phone_callback_sharp),
+                    ),
                   ],
                 ),
               )
---
+++ lib/utils/globals.dart
@@ -0,0 +1,42 @@
+library globals;
+
+// Const Global App Variables
+const String APP_NAME = "PharmaCity";
+const CITIES = <String>[
+  "Addis Ababa",
+  "Bahir Dar",
+  "Dire Dawa",
+  "Gondar",
+  "Mekele",
+  "Adama"
+      "Dessie",
+  "Hawassa",
+  "Jimma",
+  "Arba Minch",
+  "Dessie"
+];
+
+// glbal medicine details
+
+const MED_CATEGORY = <String>[
+  "Tablets",
+  "Capsules",
+  "Syrups",
+  "Solutions",
+  "Suspensions",
+  "Powders",
+  "Inhalers",
+  "Patches",
+  "Other"
+];
+
+// glbal pahrmacy details
+
+const ACC_HOLDER = <String>["Manager", "Owner", "Representative", "Other"];
+const USER_TYPE = <String>["Customer", "Admin", "Seller", "Driver"];
+const ACC_STATE = <String>["blocked", "pending", "approved", "suspended"];
+
+// Maps variable
+
+const double ADDIS_BETEL_LAT = 9.007728382765887;
+const double ADDIS_BETEL_LNG = 38.69275743275308;
